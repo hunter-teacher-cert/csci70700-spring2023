ac16f111edc1cf8e34426fcddefd18dad93aa016
Mike Zamansky <zamansky@gmail.com>
2022-01-31 18:36:18 -0500

    added TEMPLATES

--- code/flask/app.py
+++ code/flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+
 import random
 
 app = Flask(__name__)
@@ -6,14 +7,11 @@ app = Flask(__name__)
 @app.route("/rand")
 def randomnumber():
   i = random.randrange(100)
-  s = "<h1>Your lucky number is: "
-  s = s + str(i)
-  s = s + "</h1>"
-  return s
+  return render_template("lucky.html",number = i)
 
 @app.route("/")
 def index():
-  return "<h1>Hello World from Repl.it!</h1>"
+  return "<h1>Hello World from my computer !</h1>"
 
 
 app.run(host="0.0.0.0",port=5000,debug=True)
